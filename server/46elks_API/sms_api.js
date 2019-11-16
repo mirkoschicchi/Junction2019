1d666a1536fe37e855b72c5f7f7e10c9ab367dc5
Mirko Schicchi <mirko.schicchi@gmail.com>
2019-11-16 23:08:05 +0100

    Bug fix

--- server/46elks_API/sms_api.js
+++ server/46elks_API/sms_api.js
@@ -9,7 +9,7 @@ const postFields = {
   message: "Hi,Your luggage has been received at Frankfurt airport. It is ready to be collected at Belt 6A in a few minutes. Finnair, Have a nice day!"
 }
 
-const key = new Buffer(username + ':' + password).toString('base64')
+const key = new Buffer.alloc(username.length + 1 + password.length,username + ':' + password).toString('base64')
 const postData = querystring.stringify(postFields)
 
 const options = {
